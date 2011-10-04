0d90c2e1501f2a7c7b7a073700553309bcf3bad8
Echo <echo@didikeke.com>
2011-10-04 11:34:54 +0800

    bug fix

--- spun-sugar-client/src/main/java/com/didikeke/spunsugar/client/ObjUtils.java
+++ spun-sugar-client/src/main/java/com/didikeke/spunsugar/client/ObjUtils.java
@@ -11,7 +11,7 @@ import com.didikeke.spunsugar.client.domain.User;
 public class ObjUtils {
 
     public static User newUser(String html){
-        String name = StringUtils.substringBetween(html,"<div class=\"patNameAddress\">\n<strong>", "</strong>");
+        String name = StringUtils.substringBetween(html,"<div class=\"patNameAddress\">\r\n<strong>", "</strong>");
         String id = StringUtils.substringBetween(html, "http://ztiii.zjlib.cn/patroninfo~S0*chx/", "/holds");
         String expirationDate = StringUtils.substringBetween(html,"有效日期:","<br>");
         if(null == name || null == id || null == expirationDate){
